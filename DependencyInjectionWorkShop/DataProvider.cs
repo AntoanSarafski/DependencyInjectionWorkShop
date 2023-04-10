5502dc267824f2e83c52c3cc2a9ce369e1d78cad
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-04-10 23:40:07 +0300

    Real example for ServiceProvider and Dependecny Injection Container.

---
+++ DependencyInjectionWorkShop/DataProvider.cs
@@ -0,0 +1,16 @@
+﻿using ChessDependencyInversion.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessDependencyInversion
+{
+    public class DataProvider : IDataProvider
+
+    {
+        public DateTime GetDate()
+        {
+            return DateTime.Now;
+        }
+    }
+}
